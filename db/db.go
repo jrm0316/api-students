85216469a5c258042e673cac4e8a0eb2190acdda
JRM - <julianorodrigues0316@gmail.com>
2024-06-19 17:13:31 -0300

    Get students in DB

--- db/db.go
+++ db/db.go
@@ -37,3 +37,11 @@ func AddStudent(student Student) error {
 	fmt.Println("Create student!")
 	return nil
 }
+
+func GetStudents() ([]Student, error) {
+	students := []Student{}
+
+	db := Init()
+	err := db.Find(&students).Error
+	return students, err
+}
--- main.go
+++ main.go
@@ -30,7 +30,11 @@ func main() {
 
 // Handler
 func getStudents(c echo.Context) error {
-	return c.String(http.StatusOK, "List of all students")
+	students, err := db.GetStudents()
+	if err != nil {
+		return c.String(http.StatusNotFound, "Failed to get students")
+	}
+	return c.JSON(http.StatusOK, students)
 }
 
 func createStudent(c echo.Context) error {
