44c3f9c5fbdcd80096ef07f6dfc56c5ebf07e7f2
JRM - <julianorodrigues0316@gmail.com>
2024-06-19 16:06:18 -0300

    Create dynamic student in DB

--- go.mod
+++ go.mod
@@ -2,18 +2,25 @@ module github.com/jrm0316/api-students
 
 go 1.22.4
 
-require github.com/labstack/echo v3.3.10+incompatible
+require (
+	github.com/labstack/echo v3.3.10+incompatible
+	gorm.io/driver/sqlite v1.5.6
+	gorm.io/gorm v1.25.10
+)
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.24.0 // indirect
-	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/jrm0316/api-students/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
@@ -16,13 +18,44 @@ func main() {
 	e.Use(middleware.Recover())
 
 	// Routes
-	e.GET("/students", getStudent)
+	e.GET("/students", getStudents)
+	e.POST("/students", createStudent)
+	e.GET("/students/:id", getStudent)
+	e.PUT("/students/:id", updateStudent)
+	e.DELETE("/students/:id", deleteStudent)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Handler
-func getStudent(c echo.Context) error {
+func getStudents(c echo.Context) error {
 	return c.String(http.StatusOK, "List of all students")
+}
+
+func createStudent(c echo.Context) error {
+	student := db.Student{}
+	if err := c.Bind(&student); err != nil {
+		fmt.Println("Error to bind data")
+	}
+	db.AddStudent(student)
+	return c.String(http.StatusOK, "Create student")
+}
+
+func getStudent(c echo.Context) error {
+	id := c.Param("id")
+	getStud := fmt.Sprintf("Get %s student", id)
+	return c.String(http.StatusOK, getStud)
+}
+
+func updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, deleteStud)
 }
