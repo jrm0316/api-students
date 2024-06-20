6c31362fa0560fb4c4ac2a7a6bd84584c2eac4a1
JRM - <julianorodrigues0316@gmail.com>
2024-06-20 17:31:45 -0300

    Improve logs

--- db/db.go
+++ db/db.go
@@ -1,8 +1,7 @@
 package db
 
 import (
-	"fmt"
-	"log"
+	"github.com/rs/zerolog/log"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -24,7 +23,7 @@ type Student struct {
 func Init() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("student.db"), &gorm.Config{})
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatal().Err(err).Msgf("Failed to initialize SQLite: %s", err.Error())
 	}
 
 	db.AutoMigrate(&Student{})
@@ -38,9 +37,10 @@ func NewStudentHandler(db *gorm.DB) *StudentHandler {
 
 func (s *StudentHandler) AddStudent(student Student) error {
 	if result := s.DB.Create(&student); result.Error != nil {
+		log.Error().Msg("Failed to create student")
 		return result.Error
 	}
-	fmt.Println("Create student!")
+	log.Info().Msg("Create student!")
 	return nil
 }
 
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"github.com/rs/zerolog/log"
 
 	"github.com/jrm0316/api-students/api"
 )
@@ -12,6 +12,6 @@ func main() {
 	server.ConfigureRoutes()
 
 	if err := server.Start(); err != nil {
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Failed to start server")
 	}
 }
