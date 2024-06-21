5a576110db028f3772d4760d7e9f8a8aadc8e2af
JRM - <julianorodrigues0316@gmail.com>
2024-06-21 16:52:06 -0300

    Create handler to get student by ID

--- api/api.go
+++ api/api.go
@@ -1,9 +1,6 @@
 package api
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/jrm0316/api-students/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -39,43 +36,4 @@ func (api *API) ConfigureRoutes() {
 
 func (api *API) Start() error {
 	return api.Echo.Start(":8080")
-}
-
-func (api *API) getStudents(c echo.Context) error {
-	students, err := api.DB.GetStudents()
-	if err != nil {
-		return c.String(http.StatusNotFound, "Failed to get students")
-	}
-
-	return c.JSON(http.StatusOK, students)
-}
-
-func (api *API) createStudent(c echo.Context) error {
-	student := db.Student{}
-	if err := c.Bind(&student); err != nil {
-		//fmt.Println("Error to bind data")
-		return err
-	}
-	if err := api.DB.AddStudent(student); err != nil {
-		return c.String(http.StatusInternalServerError, "Error to create student")
-	}
-	return c.String(http.StatusOK, "Create student")
-}
-
-func (api *API) getStudent(c echo.Context) error {
-	id := c.Param("id")
-	getStud := fmt.Sprintf("Get %s student", id)
-	return c.String(http.StatusOK, getStud)
-}
-
-func (api *API) updateStudent(c echo.Context) error {
-	id := c.Param("id")
-	updateStud := fmt.Sprintf("Update %s student", id)
-	return c.String(http.StatusOK, updateStud)
-}
-
-func (api *API) deleteStudent(c echo.Context) error {
-	id := c.Param("id")
-	deleteStud := fmt.Sprintf("Delete %s student", id)
-	return c.String(http.StatusOK, deleteStud)
 }
---
+++ api/handler.go
@@ -0,0 +1,66 @@
+package api
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/jrm0316/api-students/db"
+	"github.com/labstack/echo"
+	"gorm.io/gorm"
+)
+
+func (api *API) getStudents(c echo.Context) error {
+	students, err := api.DB.GetStudents()
+	if err != nil {
+		return c.String(http.StatusNotFound, "Failed to get students")
+	}
+
+	return c.JSON(http.StatusOK, students)
+}
+
+func (api *API) createStudent(c echo.Context) error {
+	student := db.Student{}
+	if err := c.Bind(&student); err != nil {
+		//fmt.Println("Error to bind data")
+		return err
+	}
+	if err := api.DB.AddStudent(student); err != nil {
+		return c.String(http.StatusInternalServerError, "Error to create student")
+	}
+	return c.String(http.StatusOK, "Create student")
+}
+
+func (api *API) getStudent(c echo.Context) error {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get student ID")
+	}
+
+	student, err := api.DB.GetStudent(id)
+	// não encontrar um student com ess id -> STATUS NOT FOUND (404)
+	//ou pode ter algum problema para encontrar o student
+
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return c.String(http.StatusNotFound, "Student not found")
+	}
+
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to get student")
+	}
+
+	return c.JSON(http.StatusOK, student)
+}
+
+func (api *API) updateStudent(c echo.Context) error {
+	id := c.Param("id")
+	updateStud := fmt.Sprintf("Update %s student", id)
+	return c.String(http.StatusOK, updateStud)
+}
+
+func (api *API) deleteStudent(c echo.Context) error {
+	id := c.Param("id")
+	deleteStud := fmt.Sprintf("Delete %s student", id)
+	return c.String(http.StatusOK, deleteStud)
+}
