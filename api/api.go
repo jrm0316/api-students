a6b8fb9c47352d0d556f74edaceaaa1be37aeeeb
JRM - <julianorodrigues0316@gmail.com>
2024-06-27 17:30:31 -0300

    With Swagger

--- api/api.go
+++ api/api.go
@@ -4,12 +4,30 @@ import (
 	"github.com/jrm0316/api-students/db"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/jrm0316/students-system/docs"
 )
 
 type API struct {
 	Echo *echo.Echo
 	DB   *db.StudentHandler
 }
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server Petstore server.
+// @termsOfService http://swagger.io/terms/
+
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host petstore.swagger.io
+// @BasePath /v2
 
 func NewServer() *API {
 	e := echo.New()
@@ -32,6 +50,7 @@ func (api *API) ConfigureRoutes() {
 	api.Echo.GET("/students/:id", api.getStudent)
 	//	api.Echo.PUT("/students/:id", api.updateStudent)
 	api.Echo.DELETE("/students/:id", api.deleteStudent)
+	api.Echo.GET("/swagger/*", echoSwagger.WrapHandler)
 }
 
 func (api *API) Start() error {
